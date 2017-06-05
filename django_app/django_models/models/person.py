e25686ea563c41c066798ce302efb0c03331b9ed
junho Kwon <oilvers0904@gmail.com>
2017-06-05 12:58:56 +0900

    django_models의 models패키지화

--- django_app/django_models/models/person.py
+++ django_app/django_models/models/person.py
@@ -25,17 +25,3 @@ class Person(models.Model):
         null=True,
         blank=True,
         on_delete=models.CASCADE,)
-
-
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=40)
-
-    def __str__(self):
-        return self.name
-
-class Car(models.Model):
-    name = models.CharField(max_length=40)
-    manufacturer = models.ForeignKey(Manufacturer,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
